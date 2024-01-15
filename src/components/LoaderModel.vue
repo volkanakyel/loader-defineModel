d5bdaeee82a5e2f92c3e58c19537719e49f08650
Volkan Akyel <volkan@uikielle.com>
2024-01-16 02:49:22 +0700

    vueLoader 🌲| add loaderModel new vue 3.4 feature

---
+++ src/components/LoaderModel.vue
@@ -0,0 +1,85 @@
+<script setup>
+import { ref, watch, onUnmounted } from "vue";
+
+const props = defineProps({});
+
+const duration = defineModel("duration", { type: Number, default: 5000 });
+const isLoading = defineModel("loading", { type: Boolean, default: false });
+
+let loadingValue = ref(0);
+let intervalId = null;
+
+const startLoading = () => {
+  clearInterval(intervalId);
+  loadingValue.value = 0;
+
+  setTimeout(() => {
+    const intervalDuration = duration.value / 100;
+    intervalId = setInterval(() => {
+      if (loadingValue.value < 100) {
+        loadingValue.value += 5;
+      } else {
+        clearInterval(intervalId);
+      }
+    }, intervalDuration);
+  }, 500);
+};
+
+watch(
+  () => isLoading.value,
+  (newVal) => {
+    if (newVal) {
+      startLoading();
+    } else {
+      loadingValue.value = 0;
+      clearInterval(intervalId);
+    }
+  }
+);
+
+onUnmounted(() => {
+  clearInterval(intervalId);
+});
+</script>
+
+<template>
+  <div class="loader-container">
+    <div class="loader-classic">
+      <div class="loader-activity" :style="{ width: loadingValue + '%' }"></div>
+    </div>
+    <div class="loader-status">
+      <span>{{ loadingValue }}% </span>
+      <span v-if="loadingValue >= 100">Done</span>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.loader-container {
+  display: flex;
+  flex-direction: column;
+}
+.loader-classic {
+  width: 320px;
+  height: 12px;
+  background-color: #f0f0f0;
+  border-radius: 6px;
+  position: relative;
+}
+
+.loader-activity {
+  position: absolute;
+  height: 100%;
+  background-image: linear-gradient(to right, #0038f5, #9f03ff);
+  border-radius: 6px;
+  transition: width 0.5s ease;
+}
+
+.loader-status {
+  margin-top: 16px;
+}
+
+.section-ctas {
+  margin-top: 12px;
+}
+</style>
