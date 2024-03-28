81525a07ebf120d8fe17233e6a8e7c23e616f251
Volkan Akyel <volkan@uikielle.com>
2024-03-28 02:07:31 +0100

    add profile card and skeleton using Suspense for async behaviour

---
+++ src/views/SkeletonLoading.vue
@@ -0,0 +1,15 @@
+<template>
+  <Suspense>
+    <template #default>
+      <ProfileCard />
+    </template>
+    <template #fallback>
+      <ProfileCardSkeleton />
+    </template>
+  </Suspense>
+</template>
+
+<script setup>
+import ProfileCard from '@/components/ProfileCard.vue'
+import ProfileCardSkeleton from '@/components/ProfileCardSkeleton.vue'
+</script>
